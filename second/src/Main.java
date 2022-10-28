dbef30b1460588ad0d6fee3f7dbfe1f7e57ff0b7
Just625 <trinm@neo.vn>
2022-10-28 22:04:38 +0700

    first

---
+++ second/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        float myFloat = 3.14f;
+        int myInt = -2147483648;
+        myInt = myInt - 1;
+        System.out.println(myInt);
+        byte myByte = (byte) 128128;
+//        short myShort = 32767;
+
+        System.out.println(myByte);
+//        System.out.println(myFloat + myInt);
+//        System.out.println(myFloat - myInt);
+//        System.out.println(myFloat * myInt);
+//        System.out.println(myFloat / myInt);
+//        System.out.println(myFloat % myInt);
+//        System.out.println(1.0466667 * 3);
+//        System.out.println(5/3);
+//        System.out.println(5%3);
+//        RestTemplate
+    }
+}
